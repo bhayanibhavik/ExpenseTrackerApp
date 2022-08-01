0b7d89dbbb97245b5b2dc2e727c8faaf84e3ce03
Bhavik <bhavik.bhayani@gravitysupplychain.com>
2022-08-01 21:29:22 +0800

    Create Expnese Component
    Assignment 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: 1,
+      title: "Bike Parking Fees",
+      amount: 1235,
+      date: new Date(2022, 6, 1),
+    },
+    {
+      id: 2,
+      title: "Monthly Gas",
+      amount: 200,
+      date: new Date(2022, 6, 5),
+    },
+    {
+      id: 3,
+      title: "New Battery",
+      amount: 1200,
+      date: new Date(2022, 6, 10),
+    },
+    {
+      id: 4,
+      title: "Change of tyres",
+      amount: 3500,
+      date: new Date(2022, 6, 10),
+    },
+    {
+      id: 5,
+      title: "Tow Service",
+      amount: 400,
+      date: new Date(2022, 6, 10),
+    },
+    {
+      id: 6,
+      title: "Bike Detailing",
+      amount: 5000,
+      date: new Date(2022, 6, 15),
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
