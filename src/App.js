164fa089beba07c0f301b74722c5773605c91ac0
Bhavik Bhayani <bhavik.bhayani@gravitysupplychain.com>
2022-08-04 20:24:31 +0800

    outputting conditional content

--- src/App.js
+++ src/App.js
@@ -1,55 +1,60 @@
+import React, { useState } from "react";
 import "./App.css";
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
 
-const App = () => {
-  const expenses = [
-    {
-      id: 1,
-      title: "Bike Parking Fees",
-      amount: 1235,
-      date: new Date(2022, 6, 1),
-    },
-    {
-      id: 2,
-      title: "Monthly Gas",
-      amount: 200,
-      date: new Date(2022, 6, 5),
-    },
-    {
-      id: 3,
-      title: "New Battery",
-      amount: 1200,
-      date: new Date(2022, 6, 10),
-    },
-    {
-      id: 4,
-      title: "Change of tyres",
-      amount: 3500,
-      date: new Date(2022, 6, 10),
-    },
-    {
-      id: 5,
-      title: "Tow Service",
-      amount: 400,
-      date: new Date(2022, 6, 10),
-    },
-    {
-      id: 6,
-      title: "Bike Detailing",
-      amount: 5000,
-      date: new Date(2022, 6, 15),
-    },
-  ];
+const DUMMY_EXPENSES = [
+  {
+    id: 1,
+    title: "Bike Parking Fees",
+    amount: 1235,
+    date: new Date(2019, 6, 1),
+  },
+  {
+    id: 2,
+    title: "Monthly Gas",
+    amount: 200,
+    date: new Date(2019, 6, 5),
+  },
+  {
+    id: 3,
+    title: "New Battery",
+    amount: 1200,
+    date: new Date(2020, 6, 10),
+  },
+  {
+    id: 4,
+    title: "Change of tyres",
+    amount: 3500,
+    date: new Date(2021, 6, 10),
+  },
+  {
+    id: 5,
+    title: "Tow Service",
+    amount: 400,
+    date: new Date(2021, 6, 10),
+  },
+  {
+    id: 6,
+    title: "Bike Detailing",
+    amount: 5000,
+    date: new Date(2021, 6, 15),
+  },
+];
 
-  const addExpenseHandler = (expenseData) => {
-    console.log(expenseData);
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (newExpense) => {
+    setExpenses((prevExpense) => {
+      return [newExpense, ...prevExpense];
+    });
   };
 
   return (
     <div className="App">
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses expenses={expenses} />
+      <Expenses items={expenses} />
     </div>
   );
 };
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [displayForm, setDisplayForm] = useState(false);
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: (Math.random() * 100).toString(),
     };
     props.onAddExpense(expenseData);
+    setDisplayForm(false);
+  };
+
+  const AddExpenseHandler = (e) => {
+    setDisplayForm(true);
+  };
+
+  const CloseFormHandler = (e) => {
+    setDisplayForm(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!displayForm && (
+        <button onClick={AddExpenseHandler}>Add New Expense</button>
+      )}
+      {displayForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={CloseFormHandler}
+        />
+      )}
     </div>
   );
 };
